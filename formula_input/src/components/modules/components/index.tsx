d02f0cc12a43569776023ffa27ca6e9f4f678bd5
Pavel <medelianp@gmail.com>
2024-04-18 18:45:24 +0300

    Add: basic components structure

---
+++ formula_input/src/components/modules/components/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from "./style.module.scss"
+
+const FormulaInput = () => {
+  return (
+    <div className={styles.wrapper}>FormulaInput</div>
+  )
+}
+
+export default FormulaInput
---
+++ formula_input/src/components/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import FormulaInput from '../../modules/components'
+
+import styles from "./style.module.scss"
+
+const HomePage = () => {
+  return (
+    <div className={styles.wrapper}>
+      <FormulaInput />
+    </div>
+  )
+}
+
+export default HomePage
